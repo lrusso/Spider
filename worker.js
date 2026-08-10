a1b92601b6c6bcb08bf1f9169dab670794d343b4
Leonardo Javier Russo <info@lrusso.com>
2026-08-10 01:56:04 -0300

    Update build

--- worker.js
+++ worker.js
@@ -1,35 +1,33 @@
 const filesToCache = [
-	"Spider.htm",
-	"Spider.json",
-	"Spider.png",
-	"SpiderFavIcon_16x16.png",
-	"SpiderFavIcon_192x192.png",
-	"SpiderFavIcon_512x512.png",
-	"SpiderGame.htm",
-	"SpiderGame.js",
-	"SpiderShare.png"
-];
+  "Spider.htm",
+  "Spider.png",
+  "SpiderFavIcon_16x16.png",
+  "SpiderFavIcon_192x192.png",
+  "SpiderFavIcon_512x512.png",
+  "SpiderShare.png",
+  "manifest.json"
+]
 
-const staticCacheName = "spider-v1";
+const staticCacheName = "spider-v1"
 
 self.addEventListener("install", event => {
-	event.waitUntil(
-		caches.open(staticCacheName)
-		.then(cache => {
-			return cache.addAll(filesToCache);
-		})
-	);
-});
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache)
+    })
+  )
+})
 
 self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.match(event.request)
-		.then(response => {
-			if (response) {
-				return response;
-			}
-			return fetch(event.request)
-		}).catch(error => {
-		})
-	);
-});
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    }).catch(error => {
+    })
+  )
+})
